0f30f51f9b7d8091b6eab81605388228e731fb66
ecaycho <ecaycho@TECL-UDS008.TECSUP-LIM.LOCAL>
2014-09-13 02:01:37 -0500

    ORM-Hibernate con la BD: crm_spring
    Ejercicio 1 Lab04

--- src/main/java/com/crm/sevaweb/controllers/TestController.java
+++ src/main/java/com/crm/sevaweb/controllers/TestController.java
@@ -1,7 +1,15 @@
 
 package com.crm.sevaweb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping(value={"/","pruebas","test"})
 public class TestController {
+    
+    public String index(){
+        return "test/index";
+    }
     
 }

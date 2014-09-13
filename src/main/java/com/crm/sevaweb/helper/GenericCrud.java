b81aad6c314c41cdd75935b8ce7519c0f8dbd964
rtorres <rtorres@TECL-UDS002.TECSUP-LIM.LOCAL>
2014-09-13 11:38:43 -0500

    Clase persona Modificada con los ultimos cambios

---
+++ src/main/java/com/crm/sevaweb/helper/GenericCrud.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crm.sevaweb.helper;
+
+import java.util.List;
+
+/**
+ *
+ * @author rtorres
+ */
+public interface GenericCrud<T> {
+    
+   
+    List<T> all();
+
+    T find(Long id);
+
+    void save(T t);
+
+    void update(T t);
+
+    void delete(Long id);
+    
+}

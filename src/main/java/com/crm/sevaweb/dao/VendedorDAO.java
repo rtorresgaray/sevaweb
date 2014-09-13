d3a62e954e0c2d9ca668b5fc9b9367eda412e1ea
ecaycho <ecaycho@TECL-UDS008.TECSUP-LIM.LOCAL>
2014-09-13 11:52:39 -0500

    Clase Vendedor-Atributos HB.

---
+++ src/main/java/com/crm/sevaweb/dao/VendedorDAO.java
@@ -0,0 +1,36 @@
+package com.crm.sevaweb.dao;
+
+import com.crm.sevaweb.helper.GenericCrud;
+import com.crm.sevaweb.model.Vendedor;
+import java.util.List;
+
+public class VendedorDAO implements GenericCrud<Vendedor>{
+
+    @Override
+    public List<Vendedor> all() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Vendedor find(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void save(Vendedor t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Vendedor t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}

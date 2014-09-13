7a6d9d7f614806e6f05fd8c9bb96e39c4cbcde00
ecaycho <ecaycho@TECL-UDS008.TECSUP-LIM.LOCAL>
2014-09-13 10:54:47 -0500

    Merge branch 'master' of https://github.com/rtorresgaray/springweb.git

--- src/main/java/com/crm/sevaweb/model/Vendedor.java
+++ src/main/java/com/crm/sevaweb/model/Vendedor.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.crm.sevaweb.model;
 
-/**
- *
- * @author rtorres
- */
+
 public class Vendedor {
     
 }

7f4580329eca92668b212f29b55e04ea2a30e21d
rtorres <rtorres@TECL-UDS002.TECSUP-LIM.LOCAL>
2014-09-13 11:22:59 -0500

    Clase persona Modificada con los ultimos cambios

--- src/main/java/com/crm/sevaweb/model/Persona.java
+++ src/main/java/com/crm/sevaweb/model/Persona.java
@@ -1,13 +1,101 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.crm.sevaweb.model;
 
-/**
- *
- * @author rtorres
- */
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+    
+@Entity
+@Table(name = "programa")
 public class Persona {
     
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private long id;
+    
+    @Column(name = "nombre")
+    private String paterno;
+
+    @Column(name = "materno")
+    private String materno;
+    
+    @Column(name = "nombres")
+    private String nombres;
+    
+    @Column(name = "sexo")
+    private String sexo;
+    
+    @Column(name = "fecha_nacimiento")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fecha_nacimiento;
+    
+    @Column(name = "email")
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @OneToOne(mappedBy="persona", fetch= FetchType.LAZY)
+    private Usuario usuario;
+    
+   
 }

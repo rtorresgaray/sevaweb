7edd7101e6c86c42e05982755f412772c7074342
ecaycho <ecaycho@TECL-UDS008.TECSUP-LIM.LOCAL>
2014-09-13 11:54:41 -0500

    Clase Vendedor-Atributos HB.

--- src/main/java/com/crm/sevaweb/dao/PersonaDAO.java
+++ src/main/java/com/crm/sevaweb/dao/PersonaDAO.java
@@ -1,7 +1,33 @@
 package com.crm.sevaweb.dao;
 import com.crm.sevaweb.helper.GenericCrud;
 import com.crm.sevaweb.model.Persona;
+import java.util.List;
 
-public interface PersonaDAO extends GenericCrud<Persona>{
+public class PersonaDAO implements GenericCrud<Persona>{
+
+    @Override
+    public List<Persona> all() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Persona find(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void save(Persona t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Persona t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }

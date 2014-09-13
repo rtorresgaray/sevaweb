94cbc4b1ab2a048fa3ee96dc1f0ad3e7c9a2eb2b
ecaycho <ecaycho@TECL-UDS008.TECSUP-LIM.LOCAL>
2014-09-13 00:42:43 -0500

    Hibernate  - Clase 03

--- src/main/java/com/crm/sevaweb/model/Archivo.java
+++ src/main/java/com/crm/sevaweb/model/Archivo.java
@@ -9,5 +9,12 @@ package com.crm.sevaweb.model;
  * @author rtorres
  */
 public class Archivo {
+ 
+    private Integer idArchivo;
+    private Integer idHistoria;
+    private String ruta;
+    private String mime;
+    private Integer tamanio;
+    
     
 }

58de74d87f98a6168a72f0aa4ed515cff87fed02
rtorres <rtorres@TECL-UDS002.TECSUP-LIM.LOCAL>
2014-09-13 11:47:37 -0500

    Clase persona Modificada con los ultimos cambios

---
+++ src/main/java/com/crm/sevaweb/dao/hibernate/PersonaDAOH.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crm.sevaweb.dao.hibernate;
+
+/**
+ *
+ * @author rtorres
+ */
+public class PersonaDAOH {
+    
+}
